a89da443ada68635a6bba682b6459e499bfa43d4
Johanna Brown <johannabrown20@gmail.com>
2018-03-17 15:00:41 -0400

    step 5 changes

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -7,16 +7,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet class responsible for user registration.
- **/
+* Servlet class responsible for user registration.
+*/
+public class RegisterServlet extends HttpServlet {
 
- public class RegisterServlet extends HttpServlet {
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws IOException, ServletException {
-   
-      response.getWriter().println("<h1>RegisterServlet GET request.</h1>");
-    }
+ @Override
+ public void doGet(HttpServletRequest request, HttpServletResponse response)
+     throws IOException, ServletException {
+
+   request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
  }
+
+ @Override
+ public void doPost(HttpServletRequest request, HttpServletResponse response)
+     throws IOException, ServletException {
+
+   String username = request.getParameter("username");
+   String password = request.getParameter("password");
+
+   response.getWriter().println("<p>Username: " + username + "</p>");
+   response.getWriter().println("<p>Password: " + password + "</p>");
+ }
+}
 
  
