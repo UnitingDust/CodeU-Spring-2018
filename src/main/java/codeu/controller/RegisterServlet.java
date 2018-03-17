ba3d74b162af59f18083d531722c50e0b7db76ec
Johanna Brown <johannabrown20@gmail.com>
2018-03-17 16:10:46 -0400

    finished version for step 5

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -1,5 +1,12 @@
 package codeu.controller;
 
+
+//import the userstore class
+import codeu.model.store.basic.UserStore;
+import codeu.model.data.User;
+import java.util.UUID;
+import java.time.Instant;
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 * Servlet class responsible for user registration.
 */
 public class RegisterServlet extends HttpServlet {
+
+	 /**
+  * Store class that gives access to Users.
+  */
+ private UserStore userStore;
 
  @Override
  public void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -25,9 +37,41 @@ public class RegisterServlet extends HttpServlet {
    String username = request.getParameter("username");
    String password = request.getParameter("password");
 
-   response.getWriter().println("<p>Username: " + username + "</p>");
-   response.getWriter().println("<p>Password: " + password + "</p>");
+   if (!username.matches("[\\w*\\s*]*")) {
+     request.setAttribute("error", "Please enter only letters, numbers, and spaces.");
+     request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+     return;
+   }
+
+   if (userStore.isUserRegistered(username)) {
+     request.setAttribute("error", "That username is already taken.");
+     request.getRequestDispatcher("/WEB-INF/view/register.jsp").forward(request, response);
+     return;
+   }
+
+   User user = new User(UUID.randomUUID(), username, password, Instant.now());
+   userStore.addUser(user);
+
+   response.sendRedirect("/login");
  }
-}
+
 
  
+ /**
+  * Set up state for handling registration-related requests. This method is only called when
+  * running in a server, not when running in a test.
+  */
+ @Override
+ public void init() throws ServletException {
+   super.init();
+   setUserStore(UserStore.getInstance());
+ }
+ 
+ /**
+  * Sets the UserStore used by this servlet. This function provides a common setup method
+  * for use by the test framework or the servlet's init() function.
+  */
+ void setUserStore(UserStore userStore) {
+   this.userStore = userStore;
+ }
+}
